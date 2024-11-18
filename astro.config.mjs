75dca6ca3c9a6809d8f4d52ab3f5a23794feab77
Pskukadiya <pskukadiya98@gmail.com>
2024-11-19 00:16:59 +0530

    Commit base folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://github.com/Pskukadiya/PurvangK.github.io'
+    site: 'https://github.com/Pskukadiya/PurvangK.github.io',
+    base: 'Pskukadiya.github.io'
 });
